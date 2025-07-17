bb70610cd2c673a5af7664e34d2a3c1fafcf99f6
LeandroOliveiraVS <leandro.oliveiravs@gmail.com>
2025-07-17 14:48:58 -0300

    feat - adicionado teste para transform.py

---
+++ tests/test_transform.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import pytest
+from src.transform import Transform_Data_1
+
+def test_transform_data():
+    # Simulando um dataframe de entrada
+    df_extraido = pd.DataFrame({
+        'key_column': ['2023-01-01', '2023-01-02', None],
+        'colunas_manter': [1, 2, 3],
+        'uncolumn': [4, 5, 6]
+    })
+
+    # simulando a configuração da planilha
+    sheet_config = {
+        'table': 'test_table',
+        'key_column': 'key_column',
+        'uncolumns': ['uncolumn'],
+        'dates': ['key_column'],
+        'hours': []
+    }
+
+    # Chama a função de transformação
+    transformed_df = Transform_Data_1(df_extraido, sheet_config)
+
+    # Verifica se o DataFrame transformado tem as colunas corretas
+    assert 'key_column' in transformed_df.columns
+    # Verifica se o retorno é um DataFrame
+    assert isinstance(transformed_df, pd.DataFrame)
+    # Verifica se uma das linhas foi removida devido ao valor None na coluna chave
+    assert len(transformed_df) == 2 
+    # Verifica se as colunas não desejadas foram removidas
+    assert 'uncolumn1' not in transformed_df.columns
+    assert 'uncolumn2' not in transformed_df.columns
+    # Verifica se a coluna chave foi convertida para datetime
+    assert pd.api.types.is_datetime64_any_dtype(transformed_df['key_column'])
